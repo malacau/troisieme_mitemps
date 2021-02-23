780b164e5929641ae7e9834bcd2b661472c83690
Marc Baguet <marcbaguet@hotmail.fr>
2021-02-23 15:15:01 +0100

    Table de jointure participations entre users et ligues

--- app/controllers/ligues_controller.rb
+++ app/controllers/ligues_controller.rb
@@ -1,2 +1,6 @@
 class LiguesController < ApplicationController
+  def show
+    @ligue = Ligue.find(params[:id])
+    @participation = Participation.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :ligues, only: [:index, :show, :new, :create] do
     resources :messages, only: [:index, :create]
+    resources :participations, only: [:create, :new]
   end
   resources :line_up, only: [:show, :create, :update] do
-    
+
   end
-  
-   
+
+
 end
