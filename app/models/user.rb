a9cf9ef094e0550724f9e268e4b4243e76ab76d0
Marc Baguet <marcbaguet@hotmail.fr>
2021-02-25 11:22:06 +0100

    deux super boutons pour gagner du temps

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
-  validates :nickname, presence: true
+  validates :nickname, presence: true, uniqueness: true
   validates :photo, presence: true
-  has_many :messages 
+  has_many :messages
   has_many :participations
   has_many :line_ups
   has_many :ligues
