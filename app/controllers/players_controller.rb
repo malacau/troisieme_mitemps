3061a1aacfcd4eba05234e54c07713af71e5c22f
Antoine Puthon <antoine.p.puthon@gmail.com>
2021-02-26 12:16:57 +0100

    searchbar apparait et affiche les players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,14 @@
 class PlayersController < ApplicationController
+
   def index
     @players = Player.all
+    if !(params[:request][:name].blank?)
+      sql_query = "players.name @@ :name"
+      @players = @players.where(sql_query, name: "%#{params[:request][:name]}%")
+    end
+    if !(params[:request][:team].blank?)
+      @players = @players.joins(:team).where(teams: {name: params[:request][:team]})
+    end
+  
   end
 end
