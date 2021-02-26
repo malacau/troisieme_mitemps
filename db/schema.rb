4c873ae7c0dce07b5faf33bdeb62cfda39df294d
maxime lacaussague <maximelacaus@gmail.com>
2021-02-26 14:44:32 +0100

    add link lineup in navbar and card player updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_02_26_115201) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,8 +99,8 @@ ActiveRecord::Schema.define(version: 2021_02_26_115201) do
     t.bigint "player_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "terrain_position"
     t.integer "rating", default: 0
+    t.integer "terrain_position"
     t.index ["line_up_id"], name: "index_selections_on_line_up_id"
     t.index ["player_id"], name: "index_selections_on_player_id"
   end
