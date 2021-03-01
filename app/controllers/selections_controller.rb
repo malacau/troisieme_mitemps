6d37d79bf4bc7efcec8d24e677fef8f21681874a
Marc Baguet <marcbaguet@hotmail.fr>
2021-03-01 09:25:56 +0100

    Fixé le budget, pas de nom aux joueurs par default

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -2,9 +2,10 @@ class SelectionsController < ApplicationController
   def update
     @selection = Selection.find(params[:id])
     @player = Player.find(params[:format])
+    @selection.line_up.budget = @selection.line_up.budget + @selection.player.cote - @player.cote
     @selection.player = @player
-    @selection.line_up.budget = @selection.line_up.budget - @selection.player.cote
     @selection.line_up.save
+
     # @line_up.selections.find {|selection| selection.player == @selection.player}
     if @selection.save
       redirect_to line_up_path(@selection.line_up)
