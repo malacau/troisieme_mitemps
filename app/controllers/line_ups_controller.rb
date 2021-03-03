11765fb51ab960679919651c0d510dc28ff4881d
Marc Baguet <marcbaguet@hotmail.fr>
2021-03-03 14:28:25 +0100

    Rating des joueurs ancienne line_up

--- app/controllers/line_ups_controller.rb
+++ app/controllers/line_ups_controller.rb
@@ -3,6 +3,7 @@ class LineUpsController < ApplicationController
         @line_ups = LineUp.all
     end
     def show
+        @line_ups = current_user.line_ups
         @line_up = LineUp.find(params[:id])
         @line_up_teams_id = @line_up.teams.map(&:id)
         @teams = Team.all.map(&:name)
